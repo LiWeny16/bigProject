361de7c343eab59278639a5f187ff90791e9b25a
LiWeny16 <454888395@qq.com>
2023-02-09 16:42:58 +0800

    'update'

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports= {
     jwtSecret:'I_Love_nodeJS-0-0',
     expiresIn:'672h',
     user: ['wihsjabmxkyaigs276@gmail.com','bigonion@bigonion.cn'],
-    pass: ['scvesmwvtpnvzrsd','6LX5grmJgGBDFFhb']
+    pass: ['scvesmwvtpnvzrsd','6LX5grmJgGBDFFhb'],
+    test:[1,23,4]
 }
