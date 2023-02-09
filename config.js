f6e9b9cc3e39e3f82ac969a14fc67ddf903a3244
LiWeny16 <454888395@qq.com>
2023-02-09 16:46:19 +0800

    'update'

--- config.js
+++
@@ -1,8 +0,0 @@
-//config for jwt
-module.exports= {
-    jwtSecret:'I_Love_nodeJS-0-0',
-    expiresIn:'672h',
-    user: ['wihsjabmxkyaigs276@gmail.com','bigonion@bigonion.cn'],
-    pass: ['scvesmwvtpnvzrsd','6LX5grmJgGBDFFhb'],
-    test:[1,23,4]
-}
